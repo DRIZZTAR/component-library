327255fe921c117c18f31020aa0f5530fed39513
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-05-01 21:16:48 -0600

    added tailwind toeslint dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-slate-600 via-slate-400 to-slate-300 p-24">
