97619ea46525296539c114be718c7792daf8a012
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-05-03 20:40:47 -0600

    page bg change final gradient stop at slate-200

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-slate-600 via-slate-400 to-slate-300 p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-slate-600 via-slate-400 to-slate-200 p-24">
       <div>
-        <h1 className="text-4xl font-bold bg-white rounded-md text-center">
+        <h1 className="text-4xl font-bold bg-white/100 rounded-md text-center">
           Component Go Boom
         </h1>
       </div>
