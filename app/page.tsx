a6226bc2defdf2fc20bb9aa7333c48ca80bce8a1
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-05-04 20:12:04 -0600

    exploring same active gradient color as from color to create hard stop

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-slate-600 via-slate-400 to-slate-200 p-24">
       <div>
-        <h1 className="text-4xl font-bold bg-white/100 rounded-md text-center">
+        <h1 className="text-4xl font-bold bg-gradient-to-b from-slate-50 via-slate-50 to-slate-900 rounded-md text-center">
           Component Go Boom
         </h1>
       </div>
