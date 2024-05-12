46d247c4d19fe7fbb151d90c25e1fe0731e79d7b
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-05-11 22:20:33 -0600

    Changing meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-slate-600 via-slate-400 to-slate-200 p-24">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
       <div>
-        <h1 className="text-4xl font-bold bg-gradient-to-b from-slate-50 via-slate-50 to-slate-900 rounded-md text-center">
+        <p className="text-2xl font-bold bg-gradient-to-b from-slate-50 via-slate-50 to-slate-900 rounded-md text-center">
           Component Go Boom
-        </h1>
+        </p>
       </div>
     </main>
   );
