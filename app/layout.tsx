88d1703cad3b848f8b021b1609e7231e25fe925e
DRIZZTAR <tyson_jeremy@hotmail.com>
2024-05-14 20:13:19 -0600

    Updating comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: "Tyson Skakun Component Library",
-  description: "A collection of reusable components for Tyson Skakuns projects",
+  description: "Collection of reusable components for Tyson Skakuns projects",
 };
 
 export default function RootLayout({
